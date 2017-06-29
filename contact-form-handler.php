9ef06264d4e15e5cc32f13f4b171507a3325402d
BennyBwk <bennybwkrap@gmail.com>
2017-06-30 04:10:39 +0800

    30th June 2017 (13)

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,6 +1,9 @@
 <?php
 ini_set("SMTP","ssl://smtp.gmail.com");
 ini_set("smtp_port","465");
+ini_set("smtp_auth","true");
+ini_set("smtp_debug","1");
+ini_set("smtp_secure","ssl");
 
 $to = "bennybwkrap@gmail.com";
 $subject = "Hi!";
