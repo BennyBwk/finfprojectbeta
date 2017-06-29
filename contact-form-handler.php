bb1df9a2512b4ffd23b8f16d263c495e45e3bdf9
BennyBwk <bennybwkrap@gmail.com>
2017-06-30 02:05:49 +0800

    30th June 2017 (7)

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,12 +1,10 @@
-
-
 <?php
-// The message
-$message = "Line 1\r\nLine 2\r\nLine 3";
+$to      = 'bennybwkrap@gmail.com';
+$subject = 'the subject';
+$message = 'hello';
+$headers = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
 
-// In case any of our lines are larger than 70 characters, we should use wordwrap()
-$message = wordwrap($message, 70, "\r\n");
-
-// Send
-mail('bennybwkrap@gmail.com', 'My Subject', $message);
+mail($to, $subject, $message, $headers);
 ?>
