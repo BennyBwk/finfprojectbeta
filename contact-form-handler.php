a820c6841c6e367bb5cdeae9bbf2a53b87853819
BennyBwk <bennybwkrap@gmail.com>
2017-06-30 01:00:59 +0800

    30th June 2017 (4)
    New form method

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,29 +1,14 @@
 <?php
-$errors = '';
-$myemail = 'bennybwkrap@gmail.com';//<-----Put Your email address here.
-if(empty($_POST['nameTB'])  ||
-   empty($_POST['companyTB']) ||
-   empty($_POST['contactNoTB'])) ||
-    empty($_POST['equiryTB']))
-{
-    $errors .= "\n Error: all fields are required";
-}
-$name = $_POST['nameTB'];
-$company = $_POST['companyTB'];
-$contact_no = $_POST['contactNoTB'];
-$enquiry = $_POST['equiryTB'];
-
-if( empty($errors))
-{
-$to = $myemail;
-$email_subject = "Contact form submission from <b>$name</b>";
-$email_body = "You have received a new enquiry. ".
-" Here are the details:\n Name: $name \n ".
-"Company: $company\n Contact No: $contact_no\n Enquiry \n $enquiry";
-$headers = "From: $myemail\n";
-$headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers);
-//redirect to the 'thank you' page
-header('Location: index.html');
+if($_POST["SUBMIT"]) {
+    $recipient="bennybwkrap@gmail.com"; //Enter your mail address
+    $subject="Contact from Website"; //Subject 
+    $sender=$_POST["nameTB"];
+    $senderCompany=$_POST["companyTB"];
+    $senderContactNo=_POST["contactNoTB"];
+    $enquiry=$_POST["equiryTB"];
+    $mailBody="Name: $sender\nCompany: $senderCompany\nContact No: $senderContactNo\n\nEnquiry: $enquiry";
+    mail($recipient, $subject, $mailBody);
+	sleep(1);
+	header("Location:index.html"); // Set here redirect page or destination page
 }
 ?>
