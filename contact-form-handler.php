2eb4d38b475c6bf1636ed9adf4c1f1a8884b6f5c
BennyBwk <bennybwkrap@gmail.com>
2017-06-30 01:56:01 +0800

    30th June 2017 (6)
    Test

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,14 +1,12 @@
+
+
 <?php
-if($_POST["submit"]) {
-    $recipient="bennybwkrap@gmail.com"; //Enter your mail address
-    $subject="Contact from Website"; //Subject 
-    $sender=$_POST["nameTB"];
-    $senderCompany=$_POST["companyTB"];
-    $senderContactNo=_POST["contactNoTB"];
-    $enquiry=$_POST["equiryTB"];
-    $mailBody="Name: $sender\nCompany: $senderCompany\nContact No: $senderContactNo\n\nEnquiry: $enquiry";
-    mail($recipient, $subject, $mailBody);
-	sleep(1);
-	header("Location:index.html"); // Set here redirect page or destination page
-}
+// The message
+$message = "Line 1\r\nLine 2\r\nLine 3";
+
+// In case any of our lines are larger than 70 characters, we should use wordwrap()
+$message = wordwrap($message, 70, "\r\n");
+
+// Send
+mail('bennybwkrap@gmail.com', 'My Subject', $message);
 ?>
